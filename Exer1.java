1f409efb756a38703664d2c8ccfbd3b0ccf09fce
Peures <54548482+Peures@users.noreply.github.com>
2021-03-16 11:41:48 -0300

    Add files via upload

---
+++ Exer1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exer1;
+
+/**
+ *
+ * @author pedro
+ */
+public class Exer1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nomeCidade = "Salvador"; 
+        int anoFundacao = 1950, qntHabitantes = 3000000;
+        
+        System.out.println("A cidade " + nomeCidade + " foi fundada no ano de " + anoFundacao +  " e possui " + qntHabitantes + " de habitantes.");
+    }
+    
+}
